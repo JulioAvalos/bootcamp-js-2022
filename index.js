1c00c39cffe9f59ba4c0b1082366c8e9cad2d93e
JulioAvalos <julioavalos2@msn.com>
2022-02-21 19:09:24 -0600

    ultimo commit

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-let paragraphs = document.getElementsByTagName('p');
-console.log("parrafos en el documento:", paragraphs.length);
+const paragraphs = document.getElementsByTagName('p');
 
 if (paragraphs.length > 0) {
-  let paragraph = paragraphs[0];
+  const paragraph = paragraphs[0];
   paragraph.innerHTML = 'Bienvenidos al bootcamp!';
+}
+
+if (paragraphs.length > 1) {
+  const paragraph = paragraphs[0];
+  const fecha = new Date();
+  paragraph.innerHTML = 'parrafos en el documento: ' + paragraphs.length + "(" + fecha +")";
 }
